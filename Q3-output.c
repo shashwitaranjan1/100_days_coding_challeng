fe75a544ff905af1d7fcfc137e27a0446a391e7e
SHASHWITA RANJAN <Shashwita.28168@stu.upes.ac.in>
2025-08-30 15:57:23 +0530

    Add files via upload

---
+++ Q3-output.c
@@ -0,0 +1,9 @@
+Enter the length: 5
+Enter the breadth: 10
+The area of the rectangle of length 5 and breadth 10 is 50
+The perimeter of the rectangle of length 5 and breadth 10 is 30
+
+Enter the length: 3
+Enter the breadth: 7
+The area of the rectangle of length 3 and breadth 7 is 21
+The perimeter of the rectangle of length 3 and breadth 7 is 20
---
+++ Q4-output.c
@@ -0,0 +1,8 @@
+Enter the radius: 7
+Area = 153.94
+Circumference = 43.98
+
+
+Enter the radius: 3
+Area = 28.27
+Circumference = 18.85
