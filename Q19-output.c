5f64e8a569dd6db534c2b79628668ed723c50b2c
SHASHWITA RANJAN <Shashwita.28168@stu.upes.ac.in>
2025-08-30 17:33:17 +0530

    Add files via upload

---
+++ Q19-output.c
@@ -0,0 +1,8 @@
+Enter three sides of the triangle: 3 3 3
+Equilateral triangle
+
+Enter three sides of the triangle: 3 3 4
+Isosceles triangle
+
+Enter three sides of the triangle: 2 3 4
+Scalene triangle
---
+++ Q20-output.c
@@ -0,0 +1,5 @@
+Enter day number :1
+Monday
+
+Enter day number :5
+Friday
