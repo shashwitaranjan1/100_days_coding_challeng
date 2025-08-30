c88139191fb8d1ee664a5a3afc1862e9c34b345c
SHASHWITA RANJAN <Shashwita.28168@stu.upes.ac.in>
2025-08-30 15:21:21 +0530

    Add files via upload

---
+++ Q2-output.c
@@ -0,0 +1,13 @@
+Enter the first number: 10
+Enter the second number: 2 
+The sum of 10 and 2 is 12
+The difference of 10 and 2 is 8
+The product of 10 and 2 is 20
+The quotient of 10 and 2 is 5
+
+Enter the first number: 7
+Enter the second number: 3
+The sum of 7 and 3 is 10
+The difference of 7 and 3 is 4
+The product of 7 and 3 is 21
+The quotient of 7 and 3 is 2
