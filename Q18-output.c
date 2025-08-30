62e139bb01290da7dc40ba953a3eefcea6028a5e
SHASHWITA RANJAN <Shashwita.28168@stu.upes.ac.in>
2025-08-30 16:55:32 +0530

    Add files via upload

---
+++ Q18-output.c
@@ -0,0 +1,11 @@
+Enter the percentage: 95
+Grade A
+
+Enter the percentage: 82
+Grade B
+
+Enter the percentage: 68
+Grade D
+
+Enter the percentage: 50
+Grade F
---
+++ Q18.c
@@ -0,0 +1,29 @@
+//Write a program to assign grades based on a percentage input.
+
+#include <stdio.h>
+int main()
+{
+ int percentage;
+ printf("Enter the percentage: ");
+ scanf("%d", &percentage);
+ if (percentage >= 90)
+ {
+   printf("Grade A\n");
+ }
+ else if (percentage >= 80)
+ {
+   printf("Grade B\n");
+ }
+ else if (percentage >= 70)
+ {
+   printf("Grade C\n");
+ }
+ else if (percentage >= 60)
+ {
+   printf("Grade D\n");
+ }
+ else
+ {
+   printf("Grade F\n");
+ }
+}
