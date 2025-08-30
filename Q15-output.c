56bbbb5f4bb6fe691b011e2f5b7fde39b279a30a
SHASHWITA RANJAN <Shashwita.28168@stu.upes.ac.in>
2025-08-30 16:48:11 +0530

    Add files via upload

---
+++ Q15-output.c
@@ -0,0 +1,11 @@
+Enter a character: A
+Uppercase alphabet
+
+Enter a character: a
+Lowercase alphabet
+
+Enter a character: 0
+Digit
+
+Enter a character: #
+Special character
---
+++ Q16-output.c
@@ -0,0 +1,5 @@
+Enter three numbers: 3 7 5
+Largest = 7
+
+Enter three numbers: -1 -5 0
+Largest = 0
