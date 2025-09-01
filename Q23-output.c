047b52507a26ed493a2324eee2646ee1a5c7101c
SHASHWITA RANJAN <Shashwita.28168@stu.upes.ac.in>
2025-09-01 19:45:53 +0530

    Add files via upload

---
+++ Q23-output.c
@@ -0,0 +1,11 @@
+Enter number of late days: 4
+Fine Rs 8
+
+Enter number of late days: 8
+Fine Rs 32
+
+Enter number of late days: 15
+Fine Rs 90
+
+Enter number of late days: 31
+Membership Cancelled
