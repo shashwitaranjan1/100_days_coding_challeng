8bd7c10fc459b29ac725e244d25459208dcf90d3
SHASHWITA RANJAN <Shashwita.28168@stu.upes.ac.in>
2025-09-01 10:24:14 +0530

    Add files via upload

---
+++ Q21-output,.c
@@ -0,0 +1,5 @@
+Enter month number: 2
+February, 28 days
+
+Enter month number: 12
+December, 31 days
---
+++ Q22-output.c
@@ -0,0 +1,11 @@
+Enter Cost Price: 1000
+Enter Selling Price: 1200
+Profit Percentage = 20.00%
+
+Enter Cost Price: 1000 
+Enter Selling Price: 800
+Loss Percentage = 20.00%
+
+Enter Cost Price: 1000 
+Enter Selling Price: 1000
+No Profit, No Loss 
