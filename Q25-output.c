8b5b216ebb96765da29abf9f185ceab2091b4156
SHASHWITA RANJAN <Shashwita.28168@stu.upes.ac.in>
2025-09-04 00:21:19 +0530

    Add files via upload

---
+++ Q25-output.c
@@ -0,0 +1,13 @@
+Enter first number: 4
+Enter an operator: + 
+Enter second number: 2
+
+Enter first number: 10
+Enter an operator: %
+Enter second number: 3
+Result: 1
+
+Enter first number: 15
+Enter an operator: /
+Enter second number: 5
+Result: 3
