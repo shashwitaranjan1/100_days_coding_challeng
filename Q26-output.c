743be82bf279e0b072afce928b58a5bc1240294a
SHASHWITA RANJAN <Shashwita.28168@stu.upes.ac.in>
2025-09-04 00:27:25 +0530

    Add files via upload

---
+++ Q26-output.c
@@ -0,0 +1,5 @@
+Enter the value of n: 5
+12345
+
+Enter the value of n: 3
+123
---
+++ Q27-output.c
@@ -0,0 +1,5 @@
+Enter the value of n: 3
+Sum of first 3 odd numbers is 9
+
+Enter the value of n: 5
+Sum of first 5 odd numbers is 25
