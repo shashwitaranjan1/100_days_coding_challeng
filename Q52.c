4b01ac4df0e1c456a493a652fa3bc0cda2c39f54
SHASHWITA RANJAN <Shashwita.28168@stu.upes.ac.in>
2025-10-09 22:41:59 +0530

    Add files via upload

---
+++ Q52.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() 
+{
+  int i, j;
+  for (i = 0; i < 1; i++) 
+  {
+    printf("*\n");
+  }
+  printf("\n");
+  for (i = 0; i < 3; i++) 
+  {
+    printf("*\n");
+  }
+  printf("\n");
+  for (i = 0; i < 5; i++) 
+  {
+    printf("*\n");
+  }
+  printf("\n");
+  for (i = 0; i < 3; i++) 
+  {
+    printf("*\n");
+  }
+  printf("\n");
+  for (i = 0; i < 1; i++) 
+  {
+    printf("*\n");
+  }
+  return 0;
+}
