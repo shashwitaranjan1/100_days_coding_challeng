da388fe4a461f434fb9f079c276b77f9250f1149
SHASHWITA RANJAN <Shashwita.28168@stu.upes.ac.in>
2025-10-11 23:30:53 +0530

    Add files via upload

---
+++ Q58-output.c
@@ -0,0 +1,9 @@
+Enter the number of elements: 5
+Enter 5 elements: 2 9 1 4 7
+Maximum element: 9
+Minimum element: 1
+
+Enter the number of elements: 3
+Enter 3 elements: 10 10 10
+Maximum element: 10
+Minimum element: 10
